845a1248a81952795fc4eb309488d452b587434d
unknown <stschmaltz@gmail.com>
2017-06-18 22:37:40 -0600

    Create app layer, implement a few components, convert header to be stateless function, add routing, and have store use helper function

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './css/style.css';
-import StorePicker from './components/StorePicker'
+import { BrowserRouter, Match, Miss } from 'react-router';
 
-render(<StorePicker/>, document.querySelector('#main'));
+
+import './css/style.css';
+import App from './components/App';
+import StorePicker from './components/StorePicker'
+import NotFound from './components/NotFound'
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match exactly pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+
+render(<Root/>, document.querySelector('#main'));

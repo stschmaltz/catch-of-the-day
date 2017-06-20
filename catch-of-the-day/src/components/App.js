07bdfa4ef251bf8619a2a41c66d96345ccb8a0d6
unknown <stschmaltz@gmail.com>
2017-06-19 22:54:37 -0600

    save state for fishes and add event handling to home page and form submit

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,24 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    //Initial Statu
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // update state
+    const fishes = {...this.state.fishes};
+    // add new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // Set state
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import { getFunName } from '../helpers';
 
+
+
 class StorePicker extends React.Component{
+//  constructor() {
+//    super();
+//    this.goToStore = this.goToStore.bind(this);
+//  }
+
+  goToStore(event) {
+    event.preventDefault()
+    console.log('You Changed the Url')
+    // grab text from box
+    const storeId = this.storeInput.value
+    console.log(`Going to ${this.storeInput.value}`);
+    // transition url
+    this.context.router.transitionTo(`/store/${storeId}`);
+
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name"
-          defaultValue={getFunName()}/>
+          defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
         <button type="submit">Visit Store -></button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
  export default StorePicker;

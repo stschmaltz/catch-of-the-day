41ac7ff5353feec835159511bab2501e32fa8c4c
unknown <stschmaltz@gmail.com>
2017-06-26 00:01:36 -0600

    added the ability to update the order state when add to order button is clicked, use state in order component and display that state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
 
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
     //Initial Statu
     this.state = {
       fishes: {},
@@ -34,6 +35,14 @@ class App extends React.Component {
     });
   }
 
+  addToOrder(key) {
+    // take a copy of our state
+    const order = {...this.state.order};
+    // Update or add the new numebr of fish ordered
+    order[key] = order[key] + 1 || 1;
+    this.setState({order});
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -43,11 +52,11 @@ class App extends React.Component {
             {
               Object
               .keys(this.state.fishes)
-              .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
             }
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order}/>
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )

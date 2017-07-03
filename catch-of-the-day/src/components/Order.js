a492799bb83f1990abf52af8d8a6bbb694da4660
LAPTOP-8AP5MQNH\Shane <stschmaltz>
2017-07-02 21:33:14 -0600

    add animations to components in order (specifically the fish in the order and the count)

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { formatPrice } from '../helpers';
-
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 class Order extends React.Component {
   constructor(){
     super();
@@ -21,7 +21,18 @@ class Order extends React.Component {
 
     return (
       <li key={key}>
-        <span>{count}lbs {fish.name} {removeButton}</span>
+        <span>
+          <CSSTransitionGroup
+            component="span"
+            className="count"
+            transitionName="count"
+            transitionEnterTimeout={250}
+            transitionLeaveTimeout={250}
+            >
+            <span key={count}>{count}</span>
+          </CSSTransitionGroup>
+          lbs {fish.name} {removeButton}
+        </span>
         <span className="price">{formatPrice(count * fish.price)}</span>
       </li>
     )
@@ -41,16 +52,22 @@ class Order extends React.Component {
     return (
       <div className="order-wrap">
         <h2>Your Order</h2>
-        <ul className="order">
-          {orderIds.map(this.renderOrder)}
-          <li className="total">
-            <strong>Total:</strong>
-            {formatPrice(total)}
-          </li>
-        </ul>
-      </div>
-    )
+        <CSSTransitionGroup
+          className="order"
+          component="ul"
+          transitionName="order"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+          >
+            {orderIds.map(this.renderOrder)}
+            <li className="total">
+              <strong>Total:</strong>
+              {formatPrice(total)}
+            </li>
+          </CSSTransitionGroup>
+        </div>
+      )
+    }
   }
-}
 
-export default Order
+  export default Order

891d8a6081660bba522f7f3c13927120d88c1389
unknown <stschmaltz@gmail.com>
2017-07-04 23:02:51 -0600

    Save WIP: authentication first steps

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -5,7 +5,12 @@ class Inventory extends React.Component{
   constructor (){
     super();
     this.renderInventory = this.renderInventory.bind(this);
+    this.renderLogin = this.renderLogin.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      uid: null,
+      owner: null
+    }
   }
 
   handleChange(e, key) {
@@ -16,6 +21,17 @@ class Inventory extends React.Component{
       [e.target.name]: e.target.value
     }
     this.props.updateFish(key, updatedFish);
+  }
+
+  renderLogin() {
+    return (
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign in to manage your stores Inventory</p>
+        <button className="github" onClick={() => this.authenticate('github')}>Log In With Github</button>
+        <button className="facebook" onClick={() => this.authenticate('facebook')}>Log In With Facebook</button>
+      </nav>
+    )
   }
 
   renderInventory(key) {
@@ -39,6 +55,10 @@ class Inventory extends React.Component{
 
 
   render(){
+    // Check if they are not logged in at all
+    if(!this.state.uid) {
+      return <div>{this.renderLogin()}</div>
+    }
     return (
       <div>
         <h2>Inventory</h2>
